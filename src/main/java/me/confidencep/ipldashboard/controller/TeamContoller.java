e1a63a37090fceabda97f887cbcfb228b2807a19
confidence <confidence@hellocavalry.com>
2022-05-08 13:05:46 +0200

    Added Team API endpoint

---
+++ src/main/java/me/confidencep/ipldashboard/controller/TeamContoller.java
@@ -0,0 +1,32 @@
+package me.confidencep.ipldashboard.controller;
+
+import me.confidencep.ipldashboard.model.Team;
+import me.confidencep.ipldashboard.repository.MatchRepository;
+import me.confidencep.ipldashboard.repository.TeamRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class TeamContoller {
+
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+
+    public TeamContoller(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName) {
+        Team team = this.teamRepository.findByTeamName(teamName);
+        team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
+
+        return team;
+    }
+}
---
+++ src/main/java/me/confidencep/ipldashboard/repository/TeamRepository.java
@@ -0,0 +1,10 @@
+package me.confidencep.ipldashboard.repository;
+
+import me.confidencep.ipldashboard.model.Team;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+
+    Team findByTeamName(String teamName);
+
+}
